2b76d590d9968fa74654146cfc8c652a189c84a7
Oleksandr Prokopets <prokopets.alexandr@gmail.com>
2025-09-30 13:05:18 +0200

    Clean up sidebar items and create empty Day-Planner module

--- next-js-app/src/components/app-sidebar.tsx
+++ next-js-app/src/components/app-sidebar.tsx
@@ -1,175 +1,185 @@
-"use client"
+'use client';
 
-import * as React from "react"
-import {
-  AudioWaveform,
-  BookOpen,
-  Bot,
-  Command,
-  Frame,
-  GalleryVerticalEnd,
-  Map,
-  PieChart,
-  Settings2,
-  SquareTerminal,
-} from "lucide-react"
+import { BookOpen, Bot, Clock, Frame, Home, Map, Orbit, PieChart, Settings2, SquareTerminal } from 'lucide-react';
+import * as React from 'react';
 
-import { NavMain } from "@/components/nav-main"
-import { NavProjects } from "@/components/nav-projects"
-import { NavUser } from "@/components/nav-user"
-import { TeamSwitcher } from "@/components/team-switcher"
+import { NavUser } from '@/components/nav-user';
 import {
   Sidebar,
   SidebarContent,
   SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
   SidebarHeader,
-  SidebarRail,
-} from "@/components/ui/sidebar"
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarRail
+} from '@/components/ui/sidebar';
+import Link from 'next/link';
 
 // This is sample data.
 const data = {
   user: {
-    name: "shadcn",
-    email: "m@example.com",
-    avatar: "/avatars/shadcn.jpg",
+    name: 'shadcn',
+    email: 'm@example.com',
+    avatar: '/avatars/shadcn.jpg'
   },
-  teams: [
-    {
-      name: "Acme Inc",
-      logo: GalleryVerticalEnd,
-      plan: "Enterprise",
-    },
-    {
-      name: "Acme Corp.",
-      logo: AudioWaveform,
-      plan: "Startup",
-    },
-    {
-      name: "Evil Corp.",
-      logo: Command,
-      plan: "Free",
-    },
-  ],
   navMain: [
     {
-      title: "Playground",
-      url: "#",
+      title: 'Playground',
+      url: '#',
       icon: SquareTerminal,
-      isActive: true,
       items: [
         {
-          title: "History",
-          url: "#",
+          title: 'History',
+          url: '#'
         },
         {
-          title: "Starred",
-          url: "#",
+          title: 'Starred',
+          url: '#'
         },
         {
-          title: "Settings",
-          url: "#",
-        },
-      ],
+          title: 'Settings',
+          url: '#'
+        }
+      ]
     },
     {
-      title: "Models",
-      url: "#",
+      title: 'Models',
+      url: '#',
       icon: Bot,
       items: [
         {
-          title: "Genesis",
-          url: "#",
+          title: 'Genesis',
+          url: '#'
         },
         {
-          title: "Explorer",
-          url: "#",
+          title: 'Explorer',
+          url: '#'
         },
         {
-          title: "Quantum",
-          url: "#",
-        },
-      ],
+          title: 'Quantum',
+          url: '#'
+        }
+      ]
     },
     {
-      title: "Documentation",
-      url: "#",
+      title: 'Documentation',
+      url: '#',
       icon: BookOpen,
       items: [
         {
-          title: "Introduction",
-          url: "#",
+          title: 'Introduction',
+          url: '#'
         },
         {
-          title: "Get Started",
-          url: "#",
+          title: 'Get Started',
+          url: '#'
         },
         {
-          title: "Tutorials",
-          url: "#",
+          title: 'Tutorials',
+          url: '#'
         },
         {
-          title: "Changelog",
-          url: "#",
-        },
-      ],
+          title: 'Changelog',
+          url: '#'
+        }
+      ]
     },
     {
-      title: "Settings",
-      url: "#",
+      title: 'Settings',
+      url: '#',
       icon: Settings2,
       items: [
         {
-          title: "General",
-          url: "#",
+          title: 'General',
+          url: '#'
         },
         {
-          title: "Team",
-          url: "#",
+          title: 'Team',
+          url: '#'
         },
         {
-          title: "Billing",
-          url: "#",
+          title: 'Billing',
+          url: '#'
         },
         {
-          title: "Limits",
-          url: "#",
-        },
-      ],
-    },
+          title: 'Limits',
+          url: '#'
+        }
+      ]
+    }
   ],
   projects: [
     {
-      name: "Design Engineering",
-      url: "#",
-      icon: Frame,
+      name: 'Design Engineering',
+      url: '#',
+      icon: Frame
     },
     {
-      name: "Sales & Marketing",
-      url: "#",
-      icon: PieChart,
+      name: 'Sales & Marketing',
+      url: '#',
+      icon: PieChart
     },
     {
-      name: "Travel",
-      url: "#",
-      icon: Map,
-    },
-  ],
-}
+      name: 'Travel',
+      url: '#',
+      icon: Map
+    }
+  ]
+};
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
     <Sidebar collapsible="icon" {...props}>
       <SidebarHeader>
-        <TeamSwitcher teams={data.teams} />
+        <SidebarMenu>
+          <SidebarMenuButton
+            size="lg"
+            className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground">
+            <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
+              <Orbit className="size-4" />
+            </div>
+            <div className="leading-tight">
+              <span className="truncate font-medium">Orbit Space</span>
+            </div>
+          </SidebarMenuButton>
+        </SidebarMenu>
       </SidebarHeader>
       <SidebarContent>
-        <NavMain items={data.navMain} />
-        <NavProjects projects={data.projects} />
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild>
+                  <Link href="/">
+                    <Home />
+                    <span>Dashboard</span>
+                  </Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild>
+                  <Link href="/day-planner">
+                    <Clock />
+                    <span>Day planner</span>
+                  </Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+
+        {/* <NavMain items={data.navMain} />
+        <NavProjects projects={data.projects} /> */}
       </SidebarContent>
       <SidebarFooter>
         <NavUser user={data.user} />
       </SidebarFooter>
       <SidebarRail />
     </Sidebar>
-  )
+  );
 }
