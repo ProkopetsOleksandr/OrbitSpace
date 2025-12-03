b85aa034a09b5de4192ac99d38f315d8ebbff0d3
Oleksandr Prokopets <prokopets.alexandr@gmail.com>
2025-12-03 22:07:54 +0100

    feat (frontend): add LifeArea select form control to CreateGoalForm

--- next-js-app/src/features/goals/ui/forms/CreateGoalForm.tsx
+++ next-js-app/src/features/goals/ui/forms/CreateGoalForm.tsx
@@ -1,6 +1,8 @@
 import { goalCreateSchemaType } from '@/entities/goal/model/schemas';
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/shared/components/ui/form';
 import { Input } from '@/shared/components/ui/input';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/shared/components/ui/select';
+import { LifeArea } from '@/shared/types/api-types';
 import { GenericFormProps } from '@/shared/types/form';
 
 export const CreateGoalForm = ({ form, onSubmit, id }: GenericFormProps<goalCreateSchemaType>) => {
@@ -17,6 +19,30 @@ export const CreateGoalForm = ({ form, onSubmit, id }: GenericFormProps<goalCrea
                 <Input placeholder="Eg. Learn React" {...field} />
               </FormControl>
               <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="lifeArea"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Life Area</FormLabel>
+              <Select onValueChange={field.onChange} value={field.value || ''}>
+                <FormControl>
+                  <SelectTrigger className="w-full">
+                    <SelectValue />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  {Object.entries(LifeArea).map(([key, label]) => (
+                    <SelectItem key={key} value={key}>
+                      {label}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
             </FormItem>
           )}
         />
