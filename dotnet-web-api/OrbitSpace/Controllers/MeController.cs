fdbb3e6d94e6636c48f111622d5d541c7c846cb8
Oleksandr Prokopets <prokopets.alexandr@gmail.com>
2026-02-25 15:54:02 +0100

    feat (api): Create basic endpoint for "api/me"

---
+++ dotnet-web-api/OrbitSpace/Controllers/MeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using OrbitSpace.WebApi.Models.Responses;
+
+namespace OrbitSpace.WebApi.Controllers;
+
+[Authorize]
+[Route("api/[controller]")]
+[Tags("Me")]
+[ProducesResponseType<ProblemDetails>(StatusCodes.Status401Unauthorized)]
+public class MeController : ApiControllerBase
+{
+    private class UserDtoMock
+    {
+        public Guid Id {get; set; }
+    }
+    
+    [HttpGet]
+    [EndpointSummary("Get user info")]
+    [EndpointDescription("Returns info of currently authenticated user")]
+    [EndpointName("getMe")]
+    [ProducesResponseType<ApiResponse<UserDtoMock>>(StatusCodes.Status200OK)]
+    [ProducesResponseType<ProblemDetails>(StatusCodes.Status404NotFound)]
+    public IActionResult Me()
+    {
+        return Ok(new ApiResponse<UserDtoMock>(new UserDtoMock() { Id = CurrentUser.Id }));
+    }
+}
